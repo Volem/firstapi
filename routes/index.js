28c6ed5790675211a12177fa6f0670bb6a86c1e4
Volkan Nazmi Metin <v.metin@triodor.eu>
2017-03-07 13:51:02 +0300

    Last version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ message : "Welcome to My First API"});
+	res.json({ message : "Welcome to My First API"});
+});
+
+router.post('/', function(req, res, next) {
+	var name = req.body.name;
+	if(name){
+		res.json({ message : "Welcome to my first api " + name});
+	} else {
+		res.json({ message : "Welcome, but I don't know your name."});
+	}
 });
 
 module.exports = router;
